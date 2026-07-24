a4d1506ffe7b8e4ad705ce65991c0846b949d771
Shamaiem Shabbir <shamaiemshabbir14@gmail.com>
2026-07-24 23:26:50 +0500

    File added

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const links = {
+  shop: 'https://manabeautyspirit.com/collections/all',
+  best: 'https://manabeautyspirit.com/collections/best-selling-skins',
+  new: 'https://manabeautyspirit.com/collections/new-arrivals',
+  bundles: 'https://manabeautyspirit.com/collections/bundles',
+  sale: 'https://manabeautyspirit.com/collections/sale-flat-25-off',
+  ingredients: 'https://manabeautyspirit.com/pages/our-ingredients',
+  consultation: 'https://manabeautyspirit.com/pages/contact',
+  whatsapp: 'https://web.whatsapp.com/send?phone=+923004302111&text=Hello%2C%20I%20have%20a%20query.%20Can%20someone%20help%3F%20'
+};
+
+export const collections = [
+  { name: 'Make Up', note: 'Color', icon: 'bi bi-palette', image: 'https://manabeautyspirit.com/cdn/shop/files/Makeup_49122fd6-811a-4f64-99ac-9444650bd09f.png?v=1776792393&width=381', href: 'https://manabeautyspirit.com/collections/makeup' },
+  { name: 'Face', note: 'Cleanse', icon: 'bi bi-droplet', image: 'https://manabeautyspirit.com/cdn/shop/files/Face_1a4dc448-ffb0-4d08-b440-f2f0f97e653e.png?v=1776792393&width=380', href: 'https://manabeautyspirit.com/collections/face' },
+  { name: 'Body', note: 'Nourish', icon: 'bi bi-heart', image: 'https://manabeautyspirit.com/cdn/shop/files/Body_eb2f4783-c97f-4110-a240-4a6d788b1f2f.png?v=1776447363&width=381', href: 'https://manabeautyspirit.com/collections/body' },
+  { name: 'Hair', note: 'Grow', icon: 'bi bi-flower1', image: 'https://manabeautyspirit.com/cdn/shop/files/hair_d4c3f066-6aa5-4e6b-b346-daaabd040b22.png?v=1776792393&width=380', href: 'https://manabeautyspirit.com/collections/hair' }
+];
+
+export const bestSellers = [
+  { name: 'Multi-Correct Creamy Concealer', price: '1,275.00 PKR', old: '1,500.00 PKR', badge: '15% Off', image: 'https://manabeautyspirit.com/cdn/shop/files/Ecom1-02_344ee2ce-95c9-47e8-8221-73c46d25dd76.jpg?v=1783534784&width=2251', second: 'https://manabeautyspirit.com/cdn/shop/files/Ivory_Beige_8c65a9cc-6dcc-41ed-80cc-c3cf62e60e0d.jpg?v=1783534633&width=2251', href: 'https://manabeautyspirit.com/products/nature-correct-concealer' },
+  { name: 'Rosemary Hair Growth Spray', price: '1,000.00 PKR', old: '', badge: 'Best Seller', image: 'https://manabeautyspirit.com/cdn/shop/files/m.png?v=1759171307&width=1080', second: 'https://manabeautyspirit.com/cdn/shop/files/4277.png?v=1759171307&width=1080', href: 'https://manabeautyspirit.com/products/rosemary-hair-growth-spray' },
+  { name: 'Sweet Orange & Rosemary Shampoo', price: '1,275.00 PKR', old: '1,500.00 PKR', badge: '15% Off', image: 'https://manabeautyspirit.com/cdn/shop/files/Sweet_orange.jpg?v=1778570442&width=1080', second: 'https://manabeautyspirit.com/cdn/shop/products/manabeautyspirit-sweet-arange-rosemary-shampoo-001.jpg?v=1759171552&width=777', href: 'https://manabeautyspirit.com/products/mana-sweet-orange-and-rosemary-organic-shampoo' },
+  { name: 'Anti Blemish Face Serum Niacinamide 10% + 1% Zinc', price: '630.00 PKR', old: 'From 700.00 PKR', badge: '10% Off', image: 'https://manabeautyspirit.com/cdn/shop/files/Anti_Blemish_6aa6a5be-bc1f-4c33-ba8e-073b9ba18a5a.jpg?v=1776110912&width=1080', second: 'https://manabeautyspirit.com/cdn/shop/files/AntiBlemish10ml_1_533d0169-dc22-4dd9-a8d9-55502010205c.jpg?v=1776110912&width=1080', href: 'https://manabeautyspirit.com/products/anti-blemish-face-serum' }
+];
+
+export const ingredients = [
+  { name: 'Lavender Essential Oil', benefit: 'Calming, Soothing and Balancing', icon: 'bi bi-flower1', image: 'https://cdn.shopify.com/s/files/1/0606/1424/6606/t/3/assets/lavenderessentialoil-1636529425625_1200x.png?v=1636529445', text: 'Lavender Oil is used for its balancing and soothing qualities making it ideal for irritated skin and scalp.' },
+  { name: 'Cocoa Butter', benefit: 'Softening and Conditioning', icon: 'bi bi-heart', image: 'https://cdn.shopify.com/s/files/1/0606/1424/6606/t/3/assets/cocoabutter-1636529425608_1200x.jpg?v=1636529440', text: 'Cocoa Butter is rich in antioxidants and provides a layer of protection from skin stressors and free radicals.' },
+  { name: 'Almond Oil', benefit: 'Softening and Conditioning', icon: 'bi bi-droplet-half', image: 'https://cdn.shopify.com/s/files/1/0606/1424/6606/t/3/assets/almondoil-1636529425590_1200x.png?v=1636529436', text: 'Moisturizing and packed with Vitamin E, Almond Oil soothes and hydrates skin and adds shine to hair.' },
+  { name: 'Aloe Vera Leaf Extract', benefit: 'Soothing and Re-hydrate', icon: 'bi bi-leaf', image: 'https://cdn.shopify.com/s/files/1/0606/1424/6606/t/3/assets/aloeveraleafoil-1636529425575_1200x.png?v=1636529431', text: 'Aloe vera gel helps calm irritation and redness, re-hydrate and soften.' },
+  { name: 'Neroli Essential Oil', benefit: 'Uplifting and Toning', icon: 'bi bi-stars', image: 'https://cdn.shopify.com/s/files/1/0606/1424/6606/t/3/assets/neroliessentialoil-1636529425553_1200x.jpg?v=1636529427', text: 'Neroli has a toning, cleaning and tightening effect that helps skin look more awake and lively.' }
+];
+
+export const galleryImages = [
+  'https://manabeautyspirit.com/cdn/shop/files/WebBanner-36.jpg?v=1780401596&width=2501',
+  'https://manabeautyspirit.com/cdn/shop/files/Face_wash_Banner_Artboard_8.jpg?v=1780601463&width=2501',
+  'https://manabeautyspirit.com/cdn/shop/files/Face_wash_Banner_Artboard_3.jpg?v=1780601465&width=2501',
+  'https://manabeautyspirit.com/cdn/shop/files/Geranium.jpg?v=1778570443&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Untitled-1.png?v=1768397673&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/products/manabeautyspirit-sweet-orange-condiotner-001.jpg?v=1768397673&width=777',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-01_b984967d-6c01-4c8a-a696-9debbe6f8cb3.jpg?v=1779529002&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-02_ae5639d4-1f8e-4276-9ba0-a48c371dcdb5.jpg?v=1779529002&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/products/IMG_5496.jpg?v=1759171342&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/products/IMG_5144.jpg?v=1759171342&width=4160',
+  'https://manabeautyspirit.com/cdn/shop/files/Brilliance.jpg?v=1778570443&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/preview_images/a9f34c84f96245b194891d30103999ec.thumbnail.0000000000.jpg?v=1713861604&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-01_8d25c98f-0e32-44bb-a40a-02181d68398b.jpg?v=1779530136&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-03-03.jpg?v=1779530136&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-01_df6dd496-f3d3-4f35-95e9-ca542dacd8e2.jpg?v=1779529903&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-02_eeef2686-48ca-4bcd-82ac-b40ec42b09cb.jpg?v=1779529903&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-01.png?v=1778574155&width=4500',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-03.png?v=1778574155&width=4501',
+  'https://manabeautyspirit.com/cdn/shop/files/LipGlossOrange.jpg?v=1776966093&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/LipGlossOrangebottle.jpg?v=1776966093&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Cleansing.jpg?v=1776105062&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/Untitled-1-05-05.jpg?v=1776448185&width=4501',
+  'https://manabeautyspirit.com/cdn/shop/files/Lip_Gloss_-_Female2.jpg?v=1777310996&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/LipGlosstint.jpg?v=1774380784&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/ecom_jpg.jpg?v=1773402553&width=4658',
+  'https://manabeautyspirit.com/cdn/shop/files/Ecom-01.jpg?v=1773402553&width=4500',
+  'https://manabeautyspirit.com/cdn/shop/files/Untitled-1-02.jpg?v=1772130611&width=4501',
+  'https://manabeautyspirit.com/cdn/shop/files/DSC08006.jpg?v=1760441986&width=3504',
+  'https://manabeautyspirit.com/cdn/shop/files/shades.png?v=1771867546&width=787',
+  'https://manabeautyspirit.com/cdn/shop/files/DSC09121_2.jpg?v=1759421366&width=3504',
+  'https://manabeautyspirit.com/cdn/shop/files/SWEETORANGE.png?v=1766174726&width=800',
+  'https://manabeautyspirit.com/cdn/shop/files/DSC09066.jpg?v=1756277770&width=4672',
+  'https://manabeautyspirit.com/cdn/shop/files/DSC09086.jpg?v=1756277764&width=2500',
+  'https://manabeautyspirit.com/cdn/shop/files/rosemary.png?v=1766174319&width=800',
+  'https://manabeautyspirit.com/cdn/shop/files/Rice_Peptide.png?v=1766174599&width=800',
+  'https://manabeautyspirit.com/cdn/shop/files/Wonder.png?v=1766173998&width=800',
+  'https://manabeautyspirit.com/cdn/shop/files/4389.png?v=1756703845&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/4165.png?v=1756703845&width=1080',
+  'https://manabeautyspirit.com/cdn/shop/files/IMG_4394002.jpg?v=1756277759&width=3004',
+  'https://manabeautyspirit.com/cdn/shop/files/Frame1182.png?v=1756277759&width=1200',
+  'https://cdn.shopify.com/s/files/1/2170/1117/t/10/assets/pf-4819c1b3--Loganimg10_1200x.png?v=1626400780'
+];
